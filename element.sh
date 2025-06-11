1d8db1d2acc5aab8ddf242a96eb7fb48d3f791c6
Felipe Carvalho Sena <felipecsena877@gmail.com>
2025-06-11 00:37:04 +0000

    feat:query

--- element.sh
+++ element.sh
@@ -4,3 +4,24 @@ if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."
   exit 0
 fi
+
+ELEMENT="$1"
+
+QUERY_RESULT=$(psql --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c "
+SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius
+FROM elements
+JOIN properties USING(atomic_number)
+JOIN types USING(type_id)
+WHERE atomic_number::text = '$ELEMENT'
+   OR symbol = INITCAP('$ELEMENT')
+   OR name = INITCAP('$ELEMENT');
+")
+
+if [[ -z $QUERY_RESULT ]]; then
+  echo "I could not find that element in the database."
+  exit 0
+fi
+
+IFS='|' read -r ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELT BOIL <<< "$QUERY_RESULT"
+
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
