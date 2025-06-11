5ace79bc19a962dbb7711ded90ecf1de9160ab0d
Felipe Carvalho Sena <felipecsena877@gmail.com>
2025-06-11 00:37:40 +0000

    feat:queryResultTest

--- element.sh
+++ element.sh
@@ -22,6 +22,3 @@ if [[ -z $QUERY_RESULT ]]; then
   exit 0
 fi
 
-IFS='|' read -r ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELT BOIL <<< "$QUERY_RESULT"
-
-echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
